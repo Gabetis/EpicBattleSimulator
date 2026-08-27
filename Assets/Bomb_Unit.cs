4403e46927c1983ce870a140a63552ac377935a0
Tran Le Minh Hieu <minhhieu25478@gmail.com>
2026-08-27 15:32:21 +0700

    Add fx to bomb unit

--- Assets/Bomb_Unit.cs
+++ Assets/Bomb_Unit.cs
@@ -1,6 +1,6 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Bomb_Unit : MonoBehaviour
 {
@@ -10,22 +10,44 @@ public class Bomb_Unit : MonoBehaviour
     public GameObject explosionEffect; 
     
     private Unit unit;
+    private NavMeshAgent agent;
+    private bool hasExploded = false;
 
     void Awake()
     {
         unit = GetComponent<Unit>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (hasExploded) return;
+
+        if (unit.lives <= 0)
+        {
+            Explode(0f);
+            return;
+        }
+
+        if (unit.currentTarget != null)
+        {
+            float distance = Vector3.Distance(transform.position, unit.currentTarget.position);
+            if (distance <= agent.stoppingDistance + 0.5f)
+            {
+                Explode(0f);
+            }
+        }
     }
 
     public void Explode(float time)
     {
-     StartCoroutine(ExplodeDelayTime(time));
+        if (hasExploded) return;
+        hasExploded = true;
 
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
-        
         foreach (Collider hit in colliders)
         {
             Unit targetUnit = hit.GetComponent<Unit>();
-            
             if (targetUnit != null && targetUnit.gameObject.tag != gameObject.tag)
             {
                 targetUnit.lives -= explosionDamage;
@@ -34,14 +56,13 @@ public class Bomb_Unit : MonoBehaviour
 
         if (explosionEffect != null)
         {
-            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            GameObject fx = Instantiate(explosionEffect, transform.position + Vector3.up, Quaternion.identity);
+            ParticleSystem ps = fx.GetComponent<ParticleSystem>();
+            if (ps != null) ps.Play(true);
         }
 
+        // 3. Chết
+        unit.lives = 0;
         StartCoroutine(unit.die());
-    }
-
-    public IEnumerator ExplodeDelayTime(float time)
-    {
-        yield return new WaitForSeconds(time);
     }
 }
